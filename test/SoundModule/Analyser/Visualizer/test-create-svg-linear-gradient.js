b253231e1ec5d5aab15b75bdbb06b1c44f1a0f04
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-11-28 23:03:44 +0900

    Commit `Visualizer` test

---
+++ test/SoundModule/Analyser/Visualizer/test-create-svg-linear-gradient.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import Visualizer from '../../../../src/SoundModule/Analyser/Visualizer';
+
+describe('Visualizer TEST', () => {
+    describe('Visualizer#createSVGLinearGradient', () => {
+        const visualizer = new Visualizer(audiocontext.sampleRate);
+
+        it('should return the instance of `SVGDefsElement`', () => {
+            const defs           = visualizer.createSVGLinearGradient();
+            const linearGradient = defs.firstElementChild;
+
+            expect(defs).toEqual(jasmine.any(SVGDefsElement));
+            expect(linearGradient).toEqual(jasmine.any(SVGLinearGradientElement));
+        });
+    });
+});

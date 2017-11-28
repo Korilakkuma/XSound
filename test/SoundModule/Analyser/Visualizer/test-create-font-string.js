c5506d4f02b8ba514386aa0858b68bfe53f3e604
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-11-28 23:03:44 +0900

    Commit `Visualizer` test

---
+++ test/SoundModule/Analyser/Visualizer/test-create-font-string.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Visualizer from '../../../../src/SoundModule/Analyser/Visualizer';
+
+describe('Visualizer TEST', () => {
+    describe('Visualizer#createFontString', () => {
+        const visualizer = new Visualizer(audiocontext.sampleRate);
+
+        it('should return "13px normal normal "Arial""', () => {
+            expect(visualizer.createFontString()).toEqual('13px normal normal "Arial"');
+        });
+    });
+});
---
+++ test/SoundModule/Analyser/Visualizer/test-to-string.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Visualizer from '../../../../src/SoundModule/Analyser/Visualizer';
+
+describe('Visualizer TEST', () => {
+    describe('Visualizer#toString', () => {
+        const visualizer = new Visualizer(audiocontext.sampleRate);
+
+        it('should return "[SoundModule Analyser Visualizer]"', () => {
+            expect(visualizer.toString()).toEqual('[SoundModule Analyser Visualizer]');
+        });
+    });
+});

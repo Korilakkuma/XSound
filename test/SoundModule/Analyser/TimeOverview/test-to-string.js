2d324cef4ead8b4fca4ce90db61fe9cfa2b6ddee
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-11-28 23:06:04 +0900

    Commit `TimeOverview` test

---
+++ test/SoundModule/Analyser/TimeOverview/test-to-string.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import TimeOverview from '../../../../src/SoundModule/Analyser/TimeOverview';
+
+describe('TimeOverview TEST', () => {
+    describe('TimeOverview#toString', () => {
+        const timeOverview = new TimeOverview(audiocontext.sampleRate);
+
+        it('should return "[SoundModule Analyser TimeOverview]"', () => {
+            expect(timeOverview.toString()).toEqual('[SoundModule Analyser TimeOverview]');
+        });
+    });
+});

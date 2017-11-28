fd2d26eabd846d5427ed4d189d50d1abab1c7afe
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-11-28 22:58:54 +0900

    Commit `Analyser` test

---
+++ test/SoundModule/Analyser/test-domain.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import Analyser from '../../../src/SoundModule/Analyser';
+import TimeOverview from '../../../src/SoundModule/Analyser/TimeOverview';
+import Time from '../../../src/SoundModule/Analyser/Time';
+import FFT from '../../../src/SoundModule/Analyser/FFT';
+
+describe('Analyser TEST', () => {
+    describe('Analyser#domain', () => {
+        const analyser = new Analyser(audiocontext);
+
+        it('should return one of `TimeOverview`, `Time`, `FFT`, `Analyser`', () => {
+            expect(analyser.domain('timeOverviewL')).toEqual(jasmine.any(TimeOverview));
+            expect(analyser.domain('timeOverviewR')).toEqual(jasmine.any(TimeOverview));
+            expect(analyser.domain('time')).toEqual(jasmine.any(Time));
+            expect(analyser.domain('fft')).toEqual(jasmine.any(FFT));
+            expect(analyser.domain()).toEqual(jasmine.any(Analyser));
+            expect(analyser.domain('')).toEqual(jasmine.any(Analyser));
+        });
+    });
+});

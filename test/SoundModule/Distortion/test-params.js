b15a833b14577717a85671440efc7987263569e9
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-11-30 21:32:31 +0900

    Commit `Distortion` test

---
+++ test/SoundModule/Distortion/test-params.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import Distortion from '../../../src/SoundModule/Distortion';
+
+describe('Distortion TEST', () => {
+    describe('Distortion#params', () => {
+        const distortion = new Distortion(audiocontext, 1024);
+
+        it('should return associative array', () => {
+            expect(distortion.params()).toEqual({
+                'state'   : false,
+                'curve'   : 'clean',
+                'samples' : 4096,
+                'drive'   : 1,
+                'color'   : 350,
+                'tone'    : 350
+            });
+        });
+    });
+});

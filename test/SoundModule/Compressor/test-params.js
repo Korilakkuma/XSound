f81ff022e772a70ad07ed43d22f04a869acdd98a
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-11-30 21:27:14 +0900

    Commit `Compressor` test

---
+++ test/SoundModule/Compressor/test-params.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import Compressor from '../../../src/SoundModule/Compressor';
+
+describe('Compressor TEST', () => {
+    describe('Compressor#params', () => {
+        const compressor = new Compressor(audiocontext, 1024);
+
+        it('should return associative array', () => {
+            expect(compressor.params()).toEqual({
+                'state'     : true,
+                'threshold' : -24,
+                'knee'      : 30,
+                'ratio'     : 12,
+                'attack'    : 0.003000000026077032,
+                'release'   : 0.25
+            });
+        });
+    });
+});

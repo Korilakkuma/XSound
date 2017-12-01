e198023be9b8cf131508b92eadec7937ded37e67
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-12-01 23:35:22 +0900

    Commit `Ringmodulator` test

---
+++ test/SoundModule/Ringmodulator/test-params.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import Ringmodulator from '../../../src/SoundModule/Ringmodulator';
+
+describe('Ringmodulator TEST', () => {
+    describe('Ringmodulator#params', () => {
+        const ringmodulator = new Ringmodulator(audiocontext, 1024);
+
+        it('should return associative array', () => {
+            expect(ringmodulator.params()).toEqual({
+                'state' : false,
+                'depth' : 1,
+                'rate'  : 0
+            });
+        });
+    });
+});

b26228ec891af17c98aafaee53a867e58d4af24f
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-12-01 23:54:12 +0900

    Commit `Phaser` test

---
+++ test/SoundModule/Phaser/test-params.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import Phaser from '../../../src/SoundModule/Phaser';
+
+describe('Phaser TEST', () => {
+    describe('Phaser#params', () => {
+        const phaser = new Phaser(audiocontext, 1024);
+
+        it('should return associative array', () => {
+            expect(phaser.params()).toEqual({
+                'state'     : false,
+                'stage'     : 12,
+                'frequency' : 350,
+                'resonance' : 1,
+                'depth'     : 0,
+                'rate'      : 0,
+                'mix'       : 0,
+                'feedback'  : 0
+            });
+        });
+    });
+});

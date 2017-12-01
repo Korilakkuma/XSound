29cb12e0e267237c728e812434727d47cd9ef1a1
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-12-01 23:27:53 +0900

    Commit `Filter` test

---
+++ test/SoundModule/Filter/test-params.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import Filter from '../../../src/SoundModule/Filter';
+
+describe('Filter TEST', () => {
+    describe('Filter#params', () => {
+        const filter = new Filter(audiocontext, 1024);
+
+        it('should return associative array', () => {
+            expect(filter.params()).toEqual({
+                'state'     : false,
+                'type'      : 'lowpass',
+                'frequency' : 350,
+                'Q'         : 1,
+                'gain'      : 0,
+                'range'     : 0.1,
+                'attack'    : 0.01,
+                'decay'     : 0.3,
+                'sustain'   : 1,
+                'release'   : 1
+            });
+        });
+    });
+});

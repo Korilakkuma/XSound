bfa403da97e5f143ced758e0f0249fc4ab6eef39
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-12-01 23:27:53 +0900

    Commit `Filter` test

---
+++ test/SoundModule/Filter/test-to-string.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Filter from '../../../src/SoundModule/Filter';
+
+describe('Filter TEST', () => {
+    describe('Filter#toString', () => {
+        const filter = new Filter(audiocontext, 1024);
+
+        it('should return "[SoundModule Filter]"', () => {
+            expect(filter.toString()).toEqual('[SoundModule Filter]');
+        });
+    });
+});

6d35b57d8558ad7654789739bb9ed473f53f3ab0
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-12-02 23:52:05 +0900

    Commit `Flanger` test

---
+++ test/SoundModule/Flanger/test-to-json.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Flanger from '../../../src/SoundModule/Flanger';
+
+describe('Flanger TEST', () => {
+    describe('Flanger#toJSON', () => {
+        const flanger = new Flanger(audiocontext, 1024);
+
+        it('should return JSON', () => {
+            expect(flanger.toJSON()).toEqual('{"state":false,"time":0,"depth":0,"rate":0,"mix":0,"tone":350,"feedback":0}');
+        });
+    });
+});

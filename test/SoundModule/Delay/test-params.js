08f45f3fcdf185a136a9cfc391f0766c2520c8b8
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-12-02 23:57:00 +0900

    Commit `Delay` test

---
+++ test/SoundModule/Delay/test-params.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import Delay from '../../../src/SoundModule/Delay';
+
+describe('Delay TEST', () => {
+    describe('Delay#params', () => {
+        const delay = new Delay(audiocontext, 1024);
+
+        it('should return associative array', () => {
+            expect(delay.params()).toEqual({
+                'state'    : false,
+                'time'     : 0,
+                'dry'      : 1,
+                'wet'      : 0,
+                'tone'     : 350,
+                'feedback' : 0
+            });
+        });
+    });
+});

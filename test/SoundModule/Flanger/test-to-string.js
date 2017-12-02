992e5b47743df6a6ad866b3a04a5465fab0bab54
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-12-02 23:52:05 +0900

    Commit `Flanger` test

---
+++ test/SoundModule/Flanger/test-to-string.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Flanger from '../../../src/SoundModule/Flanger';
+
+describe('Flanger TEST', () => {
+    describe('Flanger#toString', () => {
+        const flanger = new Flanger(audiocontext, 1024);
+
+        it('should return "[SoundModule Flanger]"', () => {
+            expect(flanger.toString()).toEqual('[SoundModule Flanger]');
+        });
+    });
+});

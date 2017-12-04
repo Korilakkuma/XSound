10129e745bf0ae104fe3528a0bb17c1bbabf951c
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-12-04 21:34:06 +0900

    Commit `Oscillator` test

---
+++ test/OscillatorModule/Oscillator/test-to-string.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Oscillator from '../../../src/OscillatorModule/Oscillator';
+
+describe('Oscillator TEST', () => {
+    describe('Oscillator#toString', () => {
+        const oscillator = new Oscillator(audiocontext, true);
+
+        it('should return "[OscillatorModule Oscillator]"', () => {
+            expect(oscillator.toString()).toEqual('[OscillatorModule Oscillator]');
+        });
+    });
+});

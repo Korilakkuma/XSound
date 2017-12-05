fcdb96ce904464740ef080a43f81f230785affea
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-12-05 22:14:33 +0900

    Commit `AudioModule` test

---
+++ test/AudioModule/test-get.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import AudioModule from '../../src/AudioModule';
+
+describe('AudioModule TEST', () => {
+    describe('AudioModule#get', () => {
+        const audioModule = new AudioModule(audiocontext);
+
+        it('should return the instance of `AudioBufferSourceNode`', () => {
+            expect(audioModule.get()).toEqual(jasmine.any(AudioBufferSourceNode));
+        });
+    });
+});

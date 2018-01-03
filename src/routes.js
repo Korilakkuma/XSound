8348ae6dcafa43b11658056f8ff8b6432b47be33
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-03 17:32:22 +0900

    Commit routes

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+// import { Route } from 'react-router-dom';
+import Nav from './components/Nav';
+
+export default (
+    <nav>
+        <Nav />
+    </nav>
+);

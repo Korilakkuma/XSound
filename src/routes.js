c7832ee25e8e7446969a4a905801052cf0d7cf79
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-08 22:45:53 +0900

    Add `Header` component

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@
 
 import React from 'react';
 // import { Route } from 'react-router-dom';
+import Header from './components/Header';
 import Nav from './components/Nav';
 
 export default (
-    <nav>
+    <div>
+        <Header />
         <Nav />
-    </nav>
+    </div>
 );

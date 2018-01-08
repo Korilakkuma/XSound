8d1836184f1dce9275453ba166c016f6af0da581
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-09 08:36:50 +0900

    Fixed 'Header' component
    Correspond to Redux.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,10 +3,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import * as HeaderActions from '../../actions/HeaderActions';
 
 class Header extends React.Component {
     static CLASS_NAME  = 'Header';
     static BREAK_POINT = 768;
+
+    static propTypes = {
+        dispatch : PropTypes.func.isRequired,
+        hidden   : PropTypes.bool.isRequired
+    };
 
     constructor(props) {
         super(props);
@@ -18,27 +24,29 @@ class Header extends React.Component {
     }
 
     resize() {
+        const dispatch = this.props.dispatch;
+
         if (window.innerWidth < Header.BREAK_POINT) {
-            this.githubButtons.setAttribute('hidden', '');
-            this.forkme.setAttribute('hidden', '');
+            dispatch(HeaderActions.changeHidden(true));
         } else {
-            this.githubButtons.removeAttribute('hidden');
-            this.forkme.removeAttribute('hidden');
+            dispatch(HeaderActions.changeHidden(false));
         }
     }
 
-    shouldComponentUpdate() {
-        return true;
+    shouldComponentUpdate(nextProps) {
+        return this.props.hidden !== nextProps.hidden;
     }
 
     render() {
+        const hidden = this.props.hidden;
+
         return (
             <header className={Header.CLASS_NAME}>
-                <p ref={node => this.githubButtons = node} className={`${Header.CLASS_NAME}__github-buttons`}>
+                <p ref={node => this.githubButtons = node} hidden={hidden} className={`${Header.CLASS_NAME}__github-buttons`}>
                     <iframe src="https://ghbtns.com/github-btn.html?user=Korilakkuma&amp;repo=XSound&amp;type=star&amp;count=true&amp;size=small" frameBorder="0" scrolling="0" width="130" height="30"></iframe>
                     <iframe src="https://ghbtns.com/github-btn.html?user=Korilakkuma&amp;repo=XSound&amp;type=fork&amp;count=true&amp;size=small" frameBorder="0" scrolling="0" width="130" height="30"></iframe>
                 </p>
-                <p ref={node => this.forkme = node} className={`${Header.CLASS_NAME}__forkme`}>
+                <p ref={node => this.forkme = node} hidden={hidden} className={`${Header.CLASS_NAME}__forkme`}>
                     <a href="https://github.com/Korilakkuma/XSound" target="_blank" rel="noopener noreferrer" className="image-link">View on GitHub</a>
                 </p>
                 <h1 className={`${Header.CLASS_NAME}__title`}>XSound</h1>

592d1aafbd2f828f599c05ef06f15dbf2ee03a8d
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-10 23:10:51 +0900

    Add route for `Home`

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 'use strict';
 
 import React from 'react';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './components/Home';
 import Nav from './components/Nav';
 
 export default (
     <div>
         <Header />
-        <Nav />
+        <div className="Routes">
+            <Route exact path={location.pathname} component={Home} />
+            <Nav />
+        </div>
     </div>
 );

32cba1a3ae2dd48d2f8480013f9f71ab0f0f9af5
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-14 23:59:52 +0900

    Add `IsXSound` component

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,18 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import IsXSound from './components/xsound/is-xsound';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+
+const path = location.pathname;
 
 export default (
     <div>
         <Header />
         <div className="Routes">
-            <Route exact path={location.pathname} component={Home} />
+            <Route exact path={path} component={Home} />
+            <Route exact path={`${path}xsound/is-xsound`} component={IsXSound} />
             <Nav />
         </div>
         <Footer />

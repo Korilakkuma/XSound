5cc69270cea5d4397f1ab3f746775081cc5c0ea8
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-15 23:55:54 +0900

    Add code snippet

--- src/components/xsound/is-xsound/index.js
+++ src/components/xsound/is-xsound/index.js
@@ -14,11 +14,20 @@ class IsXSound extends React.Component {
         return false;
     }
 
+    componentWillMount() {
+        const script = document.createElement('script');
+
+        script.src   = 'https://production-assets.codepen.io/assets/embed/ei.js';
+        script.async = true;
+
+        document.body.appendChild(script);
+    }
+
     render() {
         return (
             <section>
                 <section>
-                    <h1>IS_XSOUND <span className="type">Type : <span>boolean</span></span></h1>
+                    <h1>IS_XSOUND<span className="type">Type : <span>boolean</span></span></h1>
                     <hr />
                     <p>
                         This property is to determine whether this library is usable.
@@ -27,10 +36,7 @@ class IsXSound extends React.Component {
                     </p>
                 </section>
                 <section>
-<textarea>
-console.log(&quot;X.IS_XSOUND&quot;);
-console.log(X.IS_XSOUND);
-</textarea>
+                    <p data-height="265" data-theme-id="0" data-slug-hash="NXBEjK" data-default-tab="js,result" data-user="Korilakkuma" data-embed-version="2" data-pen-title="NXBEjK" className="codepen">See the Pen <a href="https://codepen.io/Korilakkuma/pen/NXBEjK/">NXBEjK</a> by Tomohiro IKEDA (<a href="https://codepen.io/Korilakkuma">@Korilakkuma</a>) on <a href="https://codepen.io">CodePen</a>.</p>
                 </section>
             </section>
         );

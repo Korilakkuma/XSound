0c90520841196209bf5c84ff22d57f2d2a00ad32
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-16 23:12:47 +0900

    Add `SampleRate` component

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import IsXSound from './components/xsound/IsXSound';
+import SampleRate from './components/xsound/SampleRate';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 
@@ -16,6 +17,7 @@ export default (
         <div className="Routes">
             <Route exact path={path} component={Home} />
             <Route exact path={`${path}xsound/is-xsound`} component={IsXSound} />
+            <Route exact path={`${path}xsound/sample-rate`} component={SampleRate} />
             <Nav />
         </div>
         <Footer />

1c453020982dca6a4e4c6e24800dc1894b0e3fef
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-16 23:11:56 +0900

    Implement `SampleRate` component

---
+++ src/components/xsound/SampleRate/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import React from 'react';
+import { connect } from 'react-redux';
+
+class SampleRate extends React.Component {
+    static CLASS_NAME = 'SampleRate';
+
+    constructor(props) {
+        super(props);
+    }
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    render() {
+        return (
+            <section>
+                <section>
+                    <h1>SAMPLE_RATE<span className="type">Type : <span>number</span></span></h1>
+                    <hr />
+                    <p>
+                        This property is sample rate for PCM (Pulse Code Modulation).
+                    </p>
+                </section>
+                <section className="codepen">
+                    <iframe height='265' scrolling='no' title='SAMPLE_RATE | XSound - Web Audio API Library -' src='//codepen.io/Korilakkuma/embed/MrqBMp/?height=265&amp;theme-id=0&amp;default-tab=js,result&amp;embed-version=2' frameBorder='no' allowtransparency='true' allowFullScreen='true' style={{ width : '100%' }}>See the Pen <a href='https://codepen.io/Korilakkuma/pen/MrqBMp/'>SAMPLE_RATE | XSound - Web Audio API Library -</a> by Tomohiro IKEDA (<a href='https://codepen.io/Korilakkuma'>@Korilakkuma</a>) on <a href='https://codepen.io'>CodePen</a>.
+</iframe>
+                </section>
+            </section>
+        );
+    }
+}
+
+export default connect()(SampleRate);

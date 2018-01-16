974f4ddd487486aac2057b2f3679d3ee677f3275
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-16 23:16:18 +0900

    Fixed `Link`

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -48,7 +48,7 @@ class Nav extends React.Component {
                     <dl id="panel-xsound" aria-hidden={id !== 'panel-xsound'}>
                         <dt>Properties</dt>
                         <dd><Link to={`${path}xsound/is-xsound`}>IS_XSOUND</Link></dd>
-                        <dd><Link to="/xsound/sample-rate">SAMPLE_RATE</Link></dd>
+                        <dd><Link to={`${path}xsound/sample-rate`}>SAMPLE_RATE</Link></dd>
                         <dd><Link to="/xsound/number-of-inputs">NUMBER_OF_INPUTS</Link></dd>
                         <dd><Link to="/xsound/number-of-outputs">NUMBER_OF_OUTPUTS</Link></dd>
                         <dt>Methods</dt>

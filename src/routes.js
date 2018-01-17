ebcecb871f6730626cffcfe837f3e63ca655f983
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-17 21:43:04 +0900

    Add `NumberOfInputs` component

--- src/routes.js
+++ src/routes.js
@@ -6,20 +6,22 @@ import Header from './components/Header';
 import Home from './components/Home';
 import IsXSound from './components/xsound/IsXSound';
 import SampleRate from './components/xsound/SampleRate';
+import NumberOfInputs from './components/xsound/NumberOfInputs';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 
 const path = location.pathname;
 
 export default (
-    <div>
+    <React.Fragment>
         <Header />
         <div className="Routes">
             <Route exact path={path} component={Home} />
             <Route exact path={`${path}xsound/is-xsound`} component={IsXSound} />
             <Route exact path={`${path}xsound/sample-rate`} component={SampleRate} />
+            <Route exact path={`${path}xsound/number-of-inputs`} component={NumberOfInputs} />
             <Nav />
         </div>
         <Footer />
-    </div>
+    </React.Fragment>
 );

c30d2f351480a39ae0bd1ef7d8893f149e8f3a15
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-17 21:42:24 +0900

    Implement `NumberOfInputs` component

---
+++ src/components/xsound/NumberOfInputs/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import React from 'react';
+import { connect } from 'react-redux';
+
+class NumberOfInputs extends React.Component {
+    static CLASS_NAME = 'NumberOfInputs';
+
+    constructor(props) {
+        super(props);
+    }
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    render() {
+        return (
+            <section className={NumberOfInputs.CLASS_NAME}>
+                <section>
+                    <h1>SAMPLE_RATE<span className="type">Type : <span>number</span></span></h1>
+                    <hr />
+                    <p>
+                        This property is sample rate for <abbr>PCM</abbr> (Pulse Code Modulation).
+                    </p>
+                </section>
+                <section className="codepen">
+                    <iframe height='265' scrolling='no' title='NUMBER_OF_INPUTS | XSound - Web Audio API Library -' src='//codepen.io/Korilakkuma/embed/EodRWJ/?height=265&amp;theme-id=0&amp;default-tab=js,result&amp;embed-version=2' frameBorder='no' allowtransparency='true' allowFullScreen='true' style={{ width : '100%' }}>See the Pen <a href='https://codepen.io/Korilakkuma/pen/EodRWJ/'>NUMBER_OF_INPUTS | XSound - Web Audio API Library -</a> by Tomohiro IKEDA (<a href='https://codepen.io/Korilakkuma'>@Korilakkuma</a>) on <a href='https://codepen.io'>CodePen</a>.
+</iframe>
+                </section>
+            </section>
+        );
+    }
+}
+
+export default connect()(NumberOfInputs);
+

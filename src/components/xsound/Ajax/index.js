b897ed7deeff88843401af77dc8390085d4ba202
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-01-23 21:58:53 +0900

    Add description for argument and Fixed style

--- src/components/xsound/Ajax/index.js
+++ src/components/xsound/Ajax/index.js
@@ -27,8 +27,33 @@ class Ajax extends React.Component {
                         This method gets the instance of ArrayBuffer by <abbr title="Asynchronous JavaScript and XML">Ajax</abbr>.
                     </p>
                 </section>
+                <table>
+                    <caption>Arguments</caption>
+                    <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1st</th>
+                            <td><span className="argument-type">object</span></td>
+                            <td>
+                                This argument is plain object that has the following properties.
+                                <dl className="list-marker">
+                                    <dt>url <span className="argument-type">string</span></dt>
+                                    <dd>This value is resource URL.</dd>
+                                    <dt>timeout <span className="argument-type">number</span></dt>
+                                    <dd>This value is <abbr title="Asynchronous JavaScript and XML">Ajax</abbr> timeout.</dd>
+                                    <dt>success <span className="argument-type">function</span></dt>
+                                    <dd>This function is invoked on success.</dd>
+                                    <dt>error <span className="argument-type">function</span></dt>
+                                    <dd>This function is invoked on error.</dd>
+                                    <dt>progress <span className="argument-type">function</span></dt>
+                                    <dd>This function is invoked during connection.</dd>
+                                </dl>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
                 <section className="codepen">
-                    <iframe height='265' scrolling='no' title='ajax | XSound - Web Audio API Library -' src='//codepen.io/Korilakkuma/embed/PExBqJ/?height=265&amp;theme-id=0&amp;default-tab=js,result&amp;embed-version=2' frameBorder='no' allowtransparency='true' allowFullScreen='true' style={{ width : '100%'  }}>See the Pen <a href='https://codepen.io/Korilakkuma/pen/PExBqJ/'>ajax | XSound - Web Audio API Library -</a> by Tomohiro IKEDA (<a href='https://codepen.io/Korilakkuma'>@Korilakkuma</a>) on <a href='https://codepen.io'>CodePen</a>.
+                    <iframe height='265' scrolling='no' title='ajax | XSound - Web Audio API Library -' src='//codepen.io/Korilakkuma/embed/PExBqJ/?height=265&amp;theme-id=0&amp;default-tab=js,result&amp;embed-version=2' frameBorder='no' allowtransparency='true' allowFullScreen='true' style={{ width : '100%' }}>See the Pen <a href='https://codepen.io/Korilakkuma/pen/PExBqJ/'>ajax | XSound - Web Audio API Library -</a> by Tomohiro IKEDA (<a href='https://codepen.io/Korilakkuma'>@Korilakkuma</a>) on <a href='https://codepen.io'>CodePen</a>.
 </iframe>
                 </section>
             </section>

5fd01a2580b97aaea973faf5be47d5e188a52303
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-04-15 17:45:00 +0900

    Implement `MML#get` component

---
+++ src/components/mml/Get/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Get extends React.Component {
+    static CLASS_NAME = 'Get';
+
+    constructor(props) {
+        super(props);
+    }
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    render() {
+        return (
+            <section className={Get.CLASS_NAME}>
+                <section>
+                    <div className="component-title">
+                        <h1>get</h1>
+                        <p className="returns">Returns : <span>Array</span></p>
+                    </div>
+                    <hr role="presentation" />
+                    <p>
+                        This method gets the array that contains parsed MML.
+                    </p>
+                    <table>
+                        <caption>Arguments</caption>
+                        <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1st</th>
+                                <td><span className="argument-type">number</span></td>
+                                <td>This value is index for MML part.</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+                <section className="codepen">
+                    <iframe height='265' scrolling='no' title='MML#get | XSound - Web Audio API Library -' src='//codepen.io/Korilakkuma/embed/jzedKm/?height=265&amp;theme-id=0&amp;default-tab=js,result&amp;embed-version=2' frameBorder='no' allowtransparency='true' allowFullScreen='true' style={{ width : '100%' }}>See the Pen <a href='https://codepen.io/Korilakkuma/pen/jzedKm/'>MML#get | XSound - Web Audio API Library -</a> by Tomohiro IKEDA (<a href='https://codepen.io/Korilakkuma'>@Korilakkuma</a>) on <a href='https://codepen.io'>CodePen</a>.
+</iframe>
+                </section>
+            </section>
+        );
+    }
+}
+
+export default connect()(Get);

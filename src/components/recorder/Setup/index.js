1e28e341dc673932f2da5d671922a48c3ef4d848
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-05-17 21:33:12 +0900

    Implement `Recorder#setup` component

---
+++ src/components/recorder/Setup/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Setup extends React.Component {
+    static CLASS_NAME = 'Setup';
+
+    constructor(props) {
+        super(props);
+    }
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    render() {
+        return (
+            <section className={Setup.CLASS_NAME}>
+                <section>
+                    <div className="component-title">
+                        <h1>setup</h1>
+                        <p className="returns">Returns : <span>Recorder</span></p>
+                    </div>
+                    <hr role="presentation" />
+                    <p>
+                        This method setups the tracks for recording.
+                    </p>
+                    <table>
+                        <caption>Arguments</caption>
+                        <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1st</th>
+                                <td><span className="argument-type">number</span></td>
+                                <td>This value is the number of tracks for recording.</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+                <section className="codepen">
+                    <iframe height='265' scrolling='no' title='Recorder#setup | XSound - Web Audio API Library -' src='//codepen.io/Korilakkuma/embed/qYyOKg/?height=265&amp;theme-id=0&amp;default-tab=js,result&amp;embed-version=2' frameBorder='no' allowtransparency='true' allowFullScreen='true' style={{ width : '100%' }}>See the Pen <a href='https://codepen.io/Korilakkuma/pen/qYyOKg/'>Recorder#setup | XSound - Web Audio API Library -</a> by Tomohiro IKEDA (<a href='https://codepen.io/Korilakkuma'>@Korilakkuma</a>) on <a href='https://codepen.io'>CodePen</a>.
+</iframe>
+                </section>
+            </section>
+        );
+    }
+}
+
+export default connect()(Setup);

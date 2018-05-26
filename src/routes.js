e127157233b1ce7c1071eaff30face87e75ad08f
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-05-26 17:24:14 +0900

    Add `PageLoadingBar` component

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import PageLoadingBar from './components/PageLoadingBar';
 import Header from './components/Header';
 import Home from './components/Home';
 import * as XSound from './components/xsound';

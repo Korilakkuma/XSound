b2e30a4d8beb7dabd29e30e71a50ed18852b012f
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-10-25 17:27:12 +0900

    test: Run test cases in serial

--- test/XSound/test-fft.js
+++ test/XSound/test-fft.js
@@ -6,7 +6,7 @@ describe('XSound TEST', () => {
         const imags = new Float32Array([0, 0, 0, 0]);
         const size  = 4;
 
-        it('should result as FFT', () => {
+        it('should result as FFT and IFFT', () => {
             X.FFT(reals, imags, size);
 
             [1.8918883800506592, -0.9092974066734314, -0.07329356670379639, -0.9092974066734314].forEach((expected, index) => {
@@ -16,9 +16,7 @@ describe('XSound TEST', () => {
             [0, -0.7003509402275085, 0, 0.7003509402275085].forEach((expected, index) => {
                 expect(imags[index]).toBeCloseTo(expected, 3);
             });
-        });
 
-        it('should result as IFFT', () => {
             X.IFFT(reals, imags, size);
 
             [Math.sin(0), Math.sin(1), Math.sin(2), Math.sin(3)].forEach((expected, index) => {

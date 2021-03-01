528adefc92f42e1d98c04842afb7f39906a3c0bc
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-03-01 17:27:07 +0900

    refactor: Refactor to have the responsibility of parsing (Tree Construction) only

--- test/MML/TreeConstructor/test-parse.js
+++ test/MML/TreeConstructor/test-parse.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import { Token } from '../../../src/MML/Token';
 import { Tokenizer } from '../../../src/MML/Tokenizer';
 import { TreeConstructor } from '../../../src/MML/TreeConstructor';
 
@@ -8,95 +9,18 @@ describe('TreeConstructor', () => {
     const treeConstructor = new TreeConstructor(tokenizer);
 
     it('should return expected token', () => {
-        const actual   = treeConstructor.parse();
-        const expected = [
-            {
-                syntax : 'T62',
-                digits : [
-                    { digit: 62, dot: false }
-                ]
-            },
-            {
-                syntax : 'O3',
-                digits : [
-                    { digit: 3, dot: false }
-                ]
-            },
-            {
-                syntax : 'R16',
-                digits : [
-                    { digit: 16, dot: false }
-                ]
-            },
-            {
-                syntax : 'A16',
-                digits : [
-                    { digit: 16, dot: false }
-                ]
-            },
-            {
-                syntax : 'B-16',
-                digits : [
-                    { digit: 16, dot: false }
-                ]
-            },
-            {
-                syntax : 'O4',
-                digits : [
-                    { digit: 4, dot: false }
-                ]
-            },
-            {
-                syntax : 'D16',
-                digits : [
-                    { digit: 16, dot: false }
-                ]
-            },
-            {
-                syntax : 'F12',
-                digits : [
-                    { digit: 12, dot: false }
-                ]
-            },
-            {
-                syntax : 'A12',
-                digits : [
-                    { digit: 12, dot: false }
-                ]
-            },
-            {
-                syntax : 'O5',
-                digits : [
-                    { digit:  5, dot: false }
-                ]
-            },
-            {
-                syntax : 'F12',
-                digits : [
-                    { digit: 12, dot: false }
-                ]
-            },
-            {
-                syntax : 'C2&C2.',
-                digits : [
-                    { digit: 2, dot: false },
-                    { digit: 2, dot: true  }
-                ]
-            },
-            {
-                syntax : 'R4',
-                digits : [
-                    { digit: 4, dot: false }
-                ]
-            },
-            {
-                syntax : 'DB-F2.',
-                digits : [
-                    { digit: 2, dot: true }
-                ]
-            }
-        ];
+        const actualMMLs   = treeConstructor.parse();
+        const expectedMMLs = [['T', '62'], ['O', '3'], ['R', '16'], ['A', '16'], ['B-', '16'], ['O', '4'], ['D', '16'], ['F', '12'], ['A', '12'], ['O', '5'], ['F', '12'], ['C', '2'], ['&'], ['C', '2.'], ['R', '4'], ['DB-F', '2.']].map(tokens => tokens.map(t => new Token(t)));
 
-        expect(actual).toEqual(expected);
+        actualMMLs.forEach((actualMML, index) => {
+            actualMML.forEach((actual, tokenIndex) => {
+                const expected = expectedMMLs[index][tokenIndex];
+
+                expect(actual).toEqual(jasmine.any(Token));
+                expect(actual.getType()).toEqual(expected.getType());
+                expect(actual.getToken()).toEqual(expected.getToken());
+                expect(actual.getValue()).toEqual(expected.getValue());
+            });
+        });
     });
 });

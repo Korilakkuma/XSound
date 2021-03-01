5261e9a6474668044c942d74b1beacffa0c78f78
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-03-01 17:20:14 +0900

    refactor: Separate Tokenization and Tree Construction (Parsing)

--- src/MML/Tokenizer.js
+++ src/MML/Tokenizer.js
@@ -1,179 +1,89 @@
 'use strict';
 
+import { TokenMap, TokenTypes } from './TokenDefinitions';
+import { Token } from './Token';
+
 /**
  * This class is Tokenization for MML (Music Macro Language).
  * @constructor
  */
 export class Tokenizer {
-    // Token types
-    static TEMPO     = 'T';
-    static OCTAVE    = 'O';
-    static C         = 'C';
-    static D         = 'D';
-    static E         = 'E';
-    static F         = 'F';
-    static G         = 'G';
-    static A         = 'A';
-    static B         = 'B';
-    static R         = 'R';
-    static DIGITS_0  = '0';
-    static DIGITS_1  = '1';
-    static DIGITS_2  = '2';
-    static DIGITS_3  = '3';
-    static DIGITS_4  = '4';
-    static DIGITS_5  = '5';
-    static DIGITS_6  = '6';
-    static DIGITS_7  = '7';
-    static DIGITS_8  = '8';
-    static DIGITS_9  = '9';
-    static DOT       = '.';
-    static SHARP     = '#';
-    static HALF_UP   = '+';
-    static HALF_DOWN = '-';
-    static TIE       = '&';
-    static SPACES    = /\s/;  // HACK: Use RegExp ...
-    static EOS       = 'EOS';
-    static ERROR     = 'Error';
-
     /**
      * @param {string} mml This argument is MML string.
      */
     constructor(mml) {
-        if (String(mml).trim().length > 0) {
+        if (typeof mml === 'string') {
             // `reverse` for getting token by `pop`
             this.tokens = mml.trim().toUpperCase().split('').reverse();
         } else {
             this.tokens = [];
         }
 
-        this.digits = [];
-        this.symbols = [];
+        this.notes   = [];
+        this.numbers = [];
     }
 
     /**
      * This method executes tokenization from MML.
-     * @return {string} This is returned as tokenized char.
+     * @return {Token} This is returned as the instance of `Token`.
      */
     get() {
-        let token = this.trim(this.tokens.pop());
+        let token = this.tokens.pop();
 
-        // Symbols
-        switch (token) {
-            case Tokenizer.TEMPO :
-            case Tokenizer.OCTAVE:
-            case Tokenizer.C     :
-            case Tokenizer.D     :
-            case Tokenizer.E     :
-            case Tokenizer.F     :
-            case Tokenizer.G     :
-            case Tokenizer.A     :
-            case Tokenizer.B     :
-            case Tokenizer.R     :
-                this.symbols.push(token);
-
-                // EOS ?
-                if (this.tokens.length === 0) {
-                    return Tokenizer.Error;
-                }
-
-                // Look-ahead
-                token = this.trim(this.tokens.pop());
-
-                if (token === Tokenizer.SHARP) {
-                    token = Tokenizer.HALF_UP;
-                }
-
-                while (true) {
-                    switch (token) {
-                        case Tokenizer.C        :
-                        case Tokenizer.D        :
-                        case Tokenizer.E        :
-                        case Tokenizer.F        :
-                        case Tokenizer.G        :
-                        case Tokenizer.A        :
-                        case Tokenizer.B        :
-                        case Tokenizer.HALF_UP  :
-                        case Tokenizer.HALF_DOWN:
-                            this.symbols.push(token);
-
-                            // EOS ?
-                            if (this.tokens.length === 0) {
-                                return Tokenizer.ERROR;
-                            }
-
-                            // Look-ahead
-                            token = this.trim(this.tokens.pop());
-                            break;
-                        default:
-                            // Digits
-                            switch (token) {
-                                case Tokenizer.DIGITS_1:
-                                case Tokenizer.DIGITS_2:
-                                case Tokenizer.DIGITS_3:
-                                case Tokenizer.DIGITS_4:
-                                case Tokenizer.DIGITS_5:
-                                case Tokenizer.DIGITS_6:
-                                case Tokenizer.DIGITS_7:
-                                case Tokenizer.DIGITS_8:
-                                case Tokenizer.DIGITS_9:
-                                    this.digits.push(token);
-
-                                    if (this.tokens.length === 0) {
-                                        return `${this.symbols.join('')}${this.digits.join('')}`;
-                                    }
-
-                                    // Look-ahead
-                                    token = this.trim(this.tokens.pop());
-
-                                    while (true) {
-                                        switch (token) {
-                                            case Tokenizer.DIGITS_0:
-                                            case Tokenizer.DIGITS_1:
-                                            case Tokenizer.DIGITS_2:
-                                            case Tokenizer.DIGITS_3:
-                                            case Tokenizer.DIGITS_4:
-                                            case Tokenizer.DIGITS_5:
-                                            case Tokenizer.DIGITS_6:
-                                            case Tokenizer.DIGITS_7:
-                                            case Tokenizer.DIGITS_8:
-                                            case Tokenizer.DIGITS_9:
-                                            case Tokenizer.DOT     :
-                                                this.digits.push(token);
-                                                token = this.trim(this.tokens.pop());
-                                                break;
-                                            default:
-                                                // Look-back
-                                                this.tokens.push(token);
-
-                                                const s = this.symbols.join('');
-                                                const d = this.digits.join('');
-
-                                                this.symbols.length = 0;
-                                                this.digits.length  = 0;
-
-                                                return `${s}${d}`;
-                                        }
-                                    }
-                            }
-                    }
-                }
-            case Tokenizer.TIE:
-                return Tokenizer.TIE;
-            default:
-                return Tokenizer.EOS;
-        }
-    }
-
-    /**
-     * This method skips tokens that does not means as MML token.
-     * @param {string} token This argument is a token  from MML.
-     * @return {string} This is returned as token that is not white space.
-     */
-    trim(token) {
-        while (Tokenizer.SPACES.test(token)) {
+        while (TokenTypes[TokenMap.get(token)] === TokenTypes.SPACE) {
             token = this.tokens.pop();
         }
 
-        return token;
+        if (TokenTypes[TokenMap.get(token)] === TokenTypes.EOS) {
+            return new Token(token);
+        }
+
+        this.notes.length   = 0;
+        this.numbers.length = 0;
+
+        switch (TokenTypes[TokenMap.get(token)]) {
+            case TokenTypes.TEMPO:
+                return new Token(token);
+            case TokenTypes.OCTAVE:
+                return new Token(token);
+            case TokenTypes.NOTE:
+                this.notes.push(token);
+
+                // Look-ahead
+                token = this.tokens.pop();
+
+                while (TokenTypes[TokenMap.get(token)] === TokenTypes.NOTE) {
+                    this.notes.push(token);
+
+                    token = this.tokens.pop();
+                }
+
+                // Look-back
+                this.tokens.push(token);
+
+                return new Token(this.notes.join(''));
+            case TokenTypes.REST:
+                return new Token(token);
+            case TokenTypes.NUMBER:
+                this.numbers.push(token);
+
+                // Look-ahead
+                token = this.tokens.pop();
+
+                while (TokenTypes[TokenMap.get(token)] === TokenTypes.NUMBER) {
+                    this.numbers.push(token);
+
+                    token = this.tokens.pop();
+                }
+
+                // Look-back
+                this.tokens.push(token);
+
+                return new Token(this.numbers.join(''));
+            case TokenTypes.TIE:
+                return new Token(token);
+            default:
+                return new Token(token);
+        }
     }
 }
--- test/MML/Tokenizer/test-get.js
+++ test/MML/Tokenizer/test-get.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { TokenTypes } from '../../../src/MML/TokenDefinitions';
+import { Token } from '../../../src/MML/Token';
 import { Tokenizer } from '../../../src/MML/Tokenizer';
 
 describe('Tokenizer', () => {
@@ -8,19 +10,27 @@ describe('Tokenizer', () => {
 
         let actual = tokenizer.get();
 
-        const expected = ['T62', 'O3', 'R16', 'A16', 'B-16', 'O4', 'D16', 'F12', 'A12', 'O5', 'F12', 'C2', '&', 'C2.', 'R4', 'DB-F2.'];
+        const expectedTokens = ['T', '62', 'O', '3', 'R', '16', 'A', '16', 'B-', '16', 'O', '4', 'D', '16', 'F', '12', 'A', '12', 'O', '5', 'F', '12', 'C', '2', '&', 'C', '2.', 'R', '4', 'DB-F', '2.'].map(token => new Token(token));
 
-        while (actual !== Tokenizer.EOS) {
-            expect(actual).toEqual(expected.shift());
+        while (actual.getType() !== TokenTypes.EOS) {
+            const expected = expectedTokens.shift();
+
+            expect(actual).toEqual(jasmine.any(Token));
+            expect(actual.getType()).toEqual(expected.getType());
+            expect(actual.getToken()).toEqual(expected.getToken());
+            expect(actual.getValue()).toEqual(expected.getValue());
+
             actual = tokenizer.get();
         }
 
-        expect(actual).toEqual(Tokenizer.EOS);
+        expect(actual.getType()).toEqual(TokenTypes.EOS);
     });
 
     it('should return `EOS`', () => {
         const tokenizer = new Tokenizer('');
 
-        expect(tokenizer.get()).toEqual(Tokenizer.EOS);
+        const actual = tokenizer.get();
+
+        expect(actual.getType()).toEqual(TokenTypes.EOS);
     });
 });

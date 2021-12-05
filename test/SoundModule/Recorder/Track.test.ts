779dcdb048fcd40fa654a6458f15919bb2566097
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-12-05 15:56:33 +0900

    feat: Add `Track` entity class

---
+++ test/SoundModule/Recorder/Track.test.ts
@@ -0,0 +1,22 @@
+import { Track } from '../../../src/SoundModule/Recorder/Track';
+
+describe(Track.name, () => {
+  const track = new Track('track-1');
+
+  test('should append data, and clear data', () => {
+    const data1 = new Float32Array([1, 0, 1]);
+    const data2 = new Float32Array([-1, 0, -1]);
+
+    expect(track.has()).toBe(false);
+
+    track.append(data1).append(data2);
+
+    expect(track.has()).toBe(true);
+    expect(track.get()).toStrictEqual([data1, data2]);
+
+    track.clear();
+
+    expect(track.has()).toBe(false);
+    expect(track.get()).toStrictEqual([]);
+  });
+});

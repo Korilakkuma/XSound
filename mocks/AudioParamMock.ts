9d8726d44264a05d1fcce380f1b17ae14750d55d
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-12-18 00:25:43 +0900

    feat: Add `EnvelopeGenerator` (extends `Effector`) class

--- mocks/AudioParamMock.ts
+++ mocks/AudioParamMock.ts
@@ -1,8 +1,31 @@
 export class AudioParamMock {
   value = 0;
+  automationRate: 'a-rate' | 'k-rate';
 
-  constructor(value: number) {
-    this.value = value;
+  constructor(value: number, automationRate: 'a-rate' | 'k-rate' = 'a-rate') {
+    this.value          = value;
+    this.automationRate = automationRate;
+  }
+
+  setValueAtTime() {
+  }
+
+  linearRampToValueAtTime() {
+  }
+
+  exponentialRampToValueAtTime () {
+  }
+
+  setTargetAtTime() {
+  }
+
+  setValueCurveAtTime() {
+  }
+
+  cancelScheduledValues() {
+  }
+
+  cancelAndHoldAtTime() {
   }
 }
 

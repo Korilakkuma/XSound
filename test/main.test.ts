65edbf265fa7d9ae289a311aea3cd52deaa3c4a4
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-01-07 19:05:10 +0900

    feat: Implement `XSound` function
    Enable to build completely.

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,9 +1,28 @@
 import '../mocks/AudioContextMock';
+import { OscillatorModule } from '../src/OscillatorModule';
+import { OneshotModule } from '../src/OneshotModule';
+import { NoiseModule } from '../src/NoiseModule';
+import { AudioModule } from '../src/AudioModule';
+import { MediaModule } from '../src/MediaModule';
+import { StreamModule } from '../src/StreamModule';
+import { MixerModule } from '../src/MixerModule';
+import { ProcessorModule } from '../src/ProcessorModule';
+import { MIDI } from '../src/MIDI';
+import { MML } from '../src/MML';
 import { XSound } from '../src/main';
 
 describe(XSound.name, () => {
-  test('should return undefined', () => {
-    expect(XSound()).toBeUndefined();
+  test('should return instance of `Source`', () => {
+    expect(XSound('oscillator')).toBeInstanceOf(OscillatorModule);
+    expect(XSound('oneshot')).toBeInstanceOf(OneshotModule);
+    expect(XSound('noise')).toBeInstanceOf(NoiseModule);
+    expect(XSound('audio')).toBeInstanceOf(AudioModule);
+    expect(XSound('media')).toBeInstanceOf(MediaModule);
+    expect(XSound('stream')).toBeInstanceOf(StreamModule);
+    expect(XSound('processor')).toBeInstanceOf(ProcessorModule);
+    expect(XSound('mixer')).toBeInstanceOf(MixerModule);
+    expect(XSound('midi')).toBeInstanceOf(MIDI);
+    expect(XSound('mml')).toBeInstanceOf(MML);
   });
 });
 

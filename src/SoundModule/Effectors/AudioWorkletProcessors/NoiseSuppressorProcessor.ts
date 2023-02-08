5ea835b73475fac56466322cc3da6d2e3db4417e
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-02-09 08:20:25 +0900

    feat: Add classes that extend `AudioWorkletProcessor` for using AudioWorklet

---
+++ src/SoundModule/Effectors/AudioWorkletProcessors/NoiseSuppressorProcessor.ts
@@ -0,0 +1,245 @@
+import { AudioWorkletProcessor, Inputs, Outputs } from '../../../worklet';
+import { NoiseSuppressorParams } from '../NoiseSuppressor';
+
+/**
+ * This subclass is noise suppressor.
+ * @constructor
+ * @extends {AudioWorkletGlobalScope.AudioWorkletProcessor}
+ */
+export class NoiseSuppressorProcessor extends AudioWorkletProcessor {
+  /**
+   * This class (static) method executes FFT.
+   * @param {Float32Array} reals This argument is instance of `Float32Array` for real number.
+   * @param {Float32Array} imags This argument is instance of `Float32Array` for imaginary number.
+   * @param {number} size This argument is FFT size (power of two).
+   */
+  private static FFT(reals: Float32Array, imags: Float32Array, size: number): void {
+    const pow2 = (n: number) => 2 ** n;
+
+    const indexes = new Uint16Array(size);
+
+    const numberOfStages = Math.log2(size);
+
+    for (let stage = 1; stage <= numberOfStages; stage++) {
+      for (let i = 0; i < pow2(stage - 1); i++) {
+        const rest = numberOfStages - stage;
+
+        for (let j = 0; j < pow2(rest); j++) {
+          const n = i * pow2(rest + 1) + j;
+          const m = pow2(rest) + n;
+          const r = j * pow2(stage - 1);
+
+          const areal = reals[n];
+          const aimag = imags[n];
+          const breal = reals[m];
+          const bimag = imags[m];
+          const creal = Math.cos((2.0 * Math.PI * r) / size);
+          const cimag = -1 * Math.sin((2.0 * Math.PI * r) / size);
+
+          if (stage < numberOfStages) {
+            reals[n] = areal + breal;
+            imags[n] = aimag + bimag;
+            reals[m] = (creal * (areal - breal)) - (cimag * (aimag - bimag));
+            imags[m] = (creal * (aimag - bimag)) + (cimag * (areal - breal));
+          } else {
+            reals[n] = areal + breal;
+            imags[n] = aimag + bimag;
+            reals[m] = areal - breal;
+            imags[m] = aimag - bimag;
+          }
+        }
+      }
+    }
+
+    for (let stage = 1; stage <= numberOfStages; stage++) {
+      const rest = numberOfStages - stage;
+
+      for (let i = 0; i < pow2(stage - 1); i++) {
+        indexes[pow2(stage - 1) + i] = indexes[i] + pow2(rest);
+      }
+    }
+
+    for (let k = 0; k < size; k++) {
+      if (indexes[k] <= k) {
+        continue;
+      }
+
+      const real = reals[indexes[k]];
+      const imag = imags[indexes[k]];
+
+      reals[indexes[k]] = reals[k];
+      imags[indexes[k]] = imags[k];
+
+      reals[k] = real;
+      imags[k] = imag;
+    }
+  }
+
+  /**
+   * This class (static) method executes IFFT.
+   * @param {Float32Array} reals This argument is instance of `Float32Array` for real number.
+   * @param {Float32Array} imags This argument is instance of `Float32Array` for imaginary number.
+   * @param {number} size This argument is IFFT size (power of two).
+   */
+  private static IFFT(reals: Float32Array, imags: Float32Array, size: number): void {
+    const pow2 = (n: number) => 2 ** n;
+
+    const indexes = new Uint16Array(size);
+
+    const numberOfStages = Math.log2(size);
+
+    for (let stage = 1; stage <= numberOfStages; stage++) {
+      for (let i = 0; i < pow2(stage - 1); i++) {
+        const rest = numberOfStages - stage;
+
+        for (let j = 0; j < pow2(rest); j++) {
+          const n = i * pow2(rest + 1) + j;
+          const m = pow2(rest) + n;
+          const r = j * pow2(stage - 1);
+
+          const areal = reals[n];
+          const aimag = imags[n];
+          const breal = reals[m];
+          const bimag = imags[m];
+          const creal = Math.cos((2.0 * Math.PI * r) / size);
+          const cimag = Math.sin((2.0 * Math.PI * r) / size);
+
+          if (stage < numberOfStages) {
+            reals[n] = areal + breal;
+            imags[n] = aimag + bimag;
+            reals[m] = (creal * (areal - breal)) - (cimag * (aimag - bimag));
+            imags[m] = (creal * (aimag - bimag)) + (cimag * (areal - breal));
+          } else {
+            reals[n] = areal + breal;
+            imags[n] = aimag + bimag;
+            reals[m] = areal - breal;
+            imags[m] = aimag - bimag;
+          }
+        }
+      }
+    }
+
+    for (let stage = 1; stage <= numberOfStages; stage++) {
+      const rest = numberOfStages - stage;
+
+      for (let i = 0; i < pow2(stage - 1); i++) {
+        indexes[pow2(stage - 1) + i] = indexes[i] + pow2(rest);
+      }
+    }
+
+    for (let k = 0; k < size; k++) {
+      if (indexes[k] <= k) {
+        continue;
+      }
+
+      const real = reals[indexes[k]];
+      const imag = imags[indexes[k]];
+
+      reals[indexes[k]] = reals[k];
+      imags[indexes[k]] = imags[k];
+
+      reals[k] = real;
+      imags[k] = imag;
+    }
+
+    for (let k = 0; k < size; k++) {
+      reals[k] /= size;
+      imags[k] /= size;
+    }
+  }
+
+  private threshold = 0;
+  private isActive = true;
+
+  constructor() {
+    super();
+
+    this.port.onmessage = (event: MessageEvent<NoiseSuppressorParams>) => {
+      for (const [key, value] of Object.entries(event.data)) {
+        switch (key) {
+          case 'state':
+            if (typeof value === 'boolean') {
+              this.isActive = value;
+            }
+
+            break;
+          case 'threshold':
+            if (typeof value === 'number') {
+              this.threshold = value;
+            }
+
+            break;
+          default:
+            break;
+        }
+      }
+    };
+  }
+
+  /** @override */
+  protected override process(inputs: Inputs, outputs: Outputs): boolean {
+    const input  = inputs[0];
+    const output = outputs[0];
+
+    if ((input.length === 0) || (output.length === 0)) {
+      return false;
+    }
+
+    const bufferSize = input[0].length;
+
+    for (let channelNumber = 0, numberOfChannels = input.length; channelNumber < numberOfChannels; channelNumber++) {
+      this.suppress(input[channelNumber], output[channelNumber], bufferSize);
+    }
+
+    return true;
+  }
+
+  /**
+   * This method detects background noise and removes this.
+   * @param {Float32Array} inputs This argument is instance of `Float32Array` for FFT/IFFT.
+   * @param {Float32Array} outputs This argument is instance of `Float32Array` for FFT/IFFT.
+   * @param {number} fftSize This argument is FFT/IFFT size (power of two).
+   */
+  private suppress(inputs: Float32Array, outputs: Float32Array, fftSize: number): void {
+    if (!this.isActive || (this.threshold === 0)) {
+      outputs.set(inputs);
+      return;
+    }
+
+    const xreals = new Float32Array(inputs);
+    const ximags = new Float32Array(fftSize);
+
+    const yreals = new Float32Array(fftSize);
+    const yimags = new Float32Array(fftSize);
+
+    const amplitudes = new Float32Array(fftSize);
+    const phases     = new Float32Array(fftSize);
+
+    NoiseSuppressorProcessor.FFT(xreals, ximags, fftSize);
+
+    for (let k = 0; k < fftSize; k++) {
+      amplitudes[k] = Math.sqrt((xreals[k] ** 2) + (ximags[k] ** 2));
+
+      if ((xreals[k] !== 0) && (ximags[k] !== 0)) {
+        phases[k] = Math.atan2(ximags[k], xreals[k]);
+      }
+    }
+
+    for (let k = 0; k < fftSize; k++) {
+      amplitudes[k] -= this.threshold;
+
+      if (amplitudes[k] < 0) {
+        amplitudes[k] = 0;
+      }
+    }
+
+    for (let k = 0; k < fftSize; k++) {
+      yreals[k] = amplitudes[k] * Math.cos(phases[k]);
+      yimags[k] = amplitudes[k] * Math.sin(phases[k]);
+    }
+
+    NoiseSuppressorProcessor.IFFT(yreals, yimags, fftSize);
+
+    outputs.set(yreals);
+  }
+}
---
+++ src/SoundModule/Effectors/AudioWorkletProcessors/VocalCancelerProcessor.ts
@@ -0,0 +1,76 @@
+import { AudioWorkletProcessor, Inputs, Outputs } from '../../../worklet';
+import { VocalCancelerParams } from '../VocalCanceler';
+
+/**
+ * This subclass is vocal canceler.
+ * @constructor
+ * @extends {AudioWorkletGlobalScope.AudioWorkletProcessor}
+ */
+export class VocalCancelerProcessor extends AudioWorkletProcessor {
+  private depth = 0;
+  private isActive = true;
+
+  constructor() {
+    super();
+
+    this.port.onmessage = (event: MessageEvent<VocalCancelerParams>) => {
+      for (const [key, value] of Object.entries(event.data)) {
+        switch (key) {
+          case 'state':
+            if (typeof value === 'boolean') {
+              this.isActive = value;
+            }
+
+            break;
+          case 'depth':
+            if (typeof value === 'number') {
+              this.depth = value;
+            }
+
+            break;
+          default:
+            break;
+        }
+      }
+    };
+  }
+
+  /** @override */
+  protected override process(inputs: Inputs, outputs: Outputs): boolean {
+    const input  = inputs[0];
+    const output = outputs[0];
+
+    if ((input.length === 0) || (output.length === 0)) {
+      return false;
+    }
+
+    if ((input.length !== 2) || (output.length !== 2)) {
+      output[0].set(input[0]);
+
+      return true;
+    }
+
+    const bufferSize = input[0].length;
+
+    for (let i = 0; i < bufferSize; i++) {
+      output[0][i] = this.cancel(input[0][i], input[1][i]);
+      output[1][i] = this.cancel(input[1][i], input[0][i]);
+    }
+
+    return true;
+  }
+
+  /**
+   * This method removes vocal part from audio on playing.
+   * @param {number} dataL This argument is gain level for Left channel.
+   * @param {number} dataR This argument is gain level for Right channel.
+   * @return {number} Return value is audio data except vocal part.
+   */
+  private cancel(dataL: number, dataR: number): number {
+    if (this.isActive) {
+      return dataL - (this.depth * dataR);
+    }
+
+    return dataL;
+  }
+}

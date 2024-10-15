28cc2b09a02010e3aa0f9561c06b706a3528400b
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-10-15 22:40:08 +0900

    feat: FFT / IFFT C++ function

---
+++ src/SoundModule/Effectors/AudioWorkletProcessors/WebAssemblyModules/FFT.hpp
@@ -0,0 +1,140 @@
+#include <math.h>
+
+static inline int pow2(int n) {
+  if (n == 0) {
+    return 1;
+  }
+
+  return 2 << (n - 1);
+}
+
+static inline void swap(float *reals, float *imags, int i, int k) {
+  float tmp_real;
+  float tmp_imag;
+
+  tmp_real = reals[i];
+  tmp_imag = imags[i];
+
+  reals[i] = reals[k];
+  imags[i] = imags[k];
+
+  reals[k] = tmp_real;
+  imags[k] = tmp_imag;
+}
+
+static void FFT(float *reals, float *imags, int size) {
+  int number_of_stages = (int)log2f((float)size);
+
+  for (int stage = 1; stage <= number_of_stages; stage++) {
+    for (int i = 0; i < pow2(stage - 1); i++) {
+      int rest = number_of_stages - stage;
+
+      for (int j = 0; j < pow2(rest); j++) {
+        int n = i * pow2(rest + 1) + j;
+        int m = pow2(rest) + n;
+
+        float w = 2.0f * M_PI * j * pow2(stage - 1);
+
+        float e_real = reals[n];
+        float e_imag = imags[n];
+        float o_real = reals[m];
+        float o_imag = imags[m];
+        float w_real = cosf(w / size);
+        float w_imag = 0 - sinf(w / size);
+
+        if (stage < number_of_stages) {
+          reals[n] = e_real + o_real;
+          imags[n] = e_imag + o_imag;
+          reals[m] = (w_real * (e_real - o_real)) - (w_imag * (e_imag - o_imag));
+          imags[m] = (w_real * (e_imag - o_imag)) + (w_imag * (e_real - o_real));
+        } else {
+          reals[n] = e_real + o_real;
+          imags[n] = e_imag + o_imag;
+          reals[m] = e_real - o_real;
+          imags[m] = e_imag - o_imag;
+        }
+      }
+    }
+  }
+
+  int *index = (int *)calloc(size, sizeof(int));
+
+  for (int stage = 1; stage <= number_of_stages; stage++) {
+    int rest = number_of_stages - stage;
+
+    for (int i = 0; i < pow2(stage - 1); i++) {
+      index[pow2(stage - 1) + i] = index[i] + pow2(rest);
+    }
+  }
+
+  for (int k = 0; k < size; k++) {
+    if (index[k] <= k) {
+      continue;
+    }
+
+    swap(reals, imags, index[k], k);
+  }
+
+  free(index);
+}
+
+static void IFFT(float *reals, float *imags, int size) {
+  int number_of_stages = (int)log2f((float)size);
+
+  for (int stage = 1; stage <= number_of_stages; stage++) {
+    for (int i = 0; i < pow2(stage - 1); i++) {
+      int rest = number_of_stages - stage;
+
+      for (int j = 0; j < pow2(rest); j++) {
+        int n = i * pow2(rest + 1) + j;
+        int m = pow2(rest) + n;
+
+        float w = 2.0f * M_PI * j * pow2(stage - 1);
+
+        float e_real = reals[n];
+        float e_imag = imags[n];
+        float o_real = reals[m];
+        float o_imag = imags[m];
+        float w_real = cosf(w / size);
+        float w_imag = sinf(w / size);
+
+        if (stage < number_of_stages) {
+          reals[n] = e_real + o_real;
+          imags[n] = e_imag + o_imag;
+          reals[m] = (w_real * (e_real - o_real)) - (w_imag * (e_imag - o_imag));
+          imags[m] = (w_real * (e_imag - o_imag)) + (w_imag * (e_real - o_real));
+        } else {
+          reals[n] = e_real + o_real;
+          imags[n] = e_imag + o_imag;
+          reals[m] = e_real - o_real;
+          imags[m] = e_imag - o_imag;
+        }
+      }
+    }
+  }
+
+  int *index = (int *)calloc(size, sizeof(int));
+
+  for (int stage = 1; stage <= number_of_stages; stage++) {
+    int rest = number_of_stages - stage;
+
+    for (int i = 0; i < pow2(stage - 1); i++) {
+      index[pow2(stage - 1) + i] = index[i] + pow2(rest);
+    }
+  }
+
+  for (int k = 0; k < size; k++) {
+    if (index[k] <= k) {
+      continue;
+    }
+
+    swap(reals, imags, index[k], k);
+  }
+
+  for (int k = 0; k < size; k++) {
+    reals[k] /= size;
+    imags[k] /= size;
+  }
+
+  free(index);
+}

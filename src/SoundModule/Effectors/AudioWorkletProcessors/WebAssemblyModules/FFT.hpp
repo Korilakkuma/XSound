625ccc01b205c3630a6c558fccad406b551243c8
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2026-01-23 20:50:07 +0900

    chore: Fix format

--- src/SoundModule/Effectors/AudioWorkletProcessors/WebAssemblyModules/FFT.hpp
+++ src/SoundModule/Effectors/AudioWorkletProcessors/WebAssemblyModules/FFT.hpp
@@ -173,6 +173,7 @@ static void IFFT(float *const reals, float *const imags, const size_t size) {
 
     swap(reals, imags, index[k], k);
   }
+
   for (int k = 0; k < size; k++) {
     reals[k] /= size;
     imags[k] /= size;

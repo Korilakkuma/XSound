37acbc3893b3432e91cbcb07a560279dc9bc9965
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2026-04-12 10:00:03 +0900

    chore: Update playground

---
+++ playground/assets/examples/xsound/fromDecibels.js
@@ -0,0 +1,15 @@
+const sampleRate = 48000;
+const fftSize    = 32768;
+const data       = new Float32Array(fftSize);
+
+for (let n = 0; n < fftSize; n++) {
+  data[n] = Math.sin((2 * Math.PI * 440 * n) / sampleRate);
+}
+
+const amplitudes = X.fromDecibels(X.toDecibels(X.spectrum(data, 'amplitude', 'blackman')));
+
+X('analyser')
+  .domain('offline-amplitude-spectrum')
+  .setup(document.getElementById('visualizer-svg'))
+  .param({ scale: 'logarithmic', unit: 'none' })
+  .start(amplitudes);
